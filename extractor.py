d510ebfa4675c54bf697f207b0503a967fdf2151
Phil Beyer <6152278+pjbeyer@users.noreply.github.com>
2024-08-10 10:06:12 -0400

    Refactor variables; Reformat output

--- extractor.py
+++ extractor.py
@@ -14,19 +14,19 @@ lroot = ltree.getroot()
 
 for achievement in jroot.findall(".//*[@uniquename='PVE']/subcategory/achievement"):
     # Determine localized achievement description
-    name = achievement.get('name')
-    lname = lroot.find(".//*[@tuid='@" + name + "_DESCRIPTION']/tuv/seg").text
+    nameID = achievement.get('name')
+    name = lroot.find(".//*[@tuid='@" + nameID + "_DESCRIPTION']/tuv/seg").text + "\n"
 
-    # Determine localize reward description
-    reward = achievement.get('rewarditem')
-    if lroot.find(".//*[@tuid='@ITEMS_" + reward + "']/tuv/seg") is None:
-        lreward = "Item Not Found"
+    # Determine localized reward description
+    rewardID = achievement.get('rewarditem')
+    if lroot.find(".//*[@tuid='@ITEMS_" + rewardID + "']/tuv/seg") is None:
+        reward = "Item Not Found"
     else:
-        lreward = lroot.find(".//*[@tuid='@ITEMS_" + reward + "']/tuv/seg").text
+        reward = lroot.find(".//*[@tuid='@ITEMS_" + rewardID + "']/tuv/seg").text
 
     # Append reward amount where present
     amount = 1 if achievement.get('rewardamount') is None else achievement.get('rewardamount')
-    lreward = lreward if amount == 1 else lreward + " (x" + amount + ")"
+    reward = reward if amount == 1 else reward + " (x" + amount + ")"
 
-    # print(name,reward,amount)
-    print(lname,lreward)
+    # print(nameID,rewardID,amount)
+    print(name,"<Reward id=\"" + rewardID + "\" title=\"" + reward + "\" />")
